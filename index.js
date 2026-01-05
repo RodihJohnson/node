8b092c0aae93bd90389ea62b82a1e68b04e8570b
RodihJohnson <meyerbronson9@gmail.com>
2026-01-04 18:53:00 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const app = express();
 
+const PORT = process.env.PORT || 10000;
+
+const SERVERS = [
+  { id: "1111111111", players: 1, brainrot: "Strawberry Elephant", value: 250000000 },
+  { id: "2222222222", players: 2, brainrot: "Dragon Cannelloni", value: 100000000 }
+];
+
+app.get("/", (req, res) => {
+  res.send("Backend online");
+});
+
+app.get("/servers", (req, res) => {
+  res.json(SERVERS);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on port ${PORT}`);
+});
